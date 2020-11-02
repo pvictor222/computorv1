89531498cb98470590cb9c25709ed54963e28612
Paul VICTOR <p.victor@outlook.fr>
2020-11-02 14:55:24 +0100

    Check that all characters in the input are valid

--- README.md
+++ README.md
@@ -5,10 +5,11 @@ Project from 42 School
 
 *BONUS*
 - Check the number of inputs
+- Checks the formatting of input for obvious errors
 - Processing natural language equations
 - Visualization (only if the user asks for it)
 - Visualization : asks the user for left and right limits and padding
-- Visualization : asks the user for format (pdf of png)
+- Visualization : asks the user for output format (pdf of png)
 - Asks the user if he wants to print the discriminant
 - If no input, asks for input
 
--- src/check_polynom.jl
+++ src/check_polynom.jl
@@ -3,21 +3,39 @@ include("reduce.jl")
 include("print_reduced.jl")
 
 #=
+    Checks each char in the string to see if they are legit 
+=#
+function check_chars(string)
+    valid = 1
+    for i in string
+        valid = (occursin(i, "012345678-+=* X^") == true) ? valid : -1
+    end
+    if (valid == -1)
+        println("Wrong characters have been detected. Please enter a polynomial function of the form: 'a * X^0 + b * X^1 + c * X^2'")
+    end
+    return (valid)
+end
+
+#=
     Send to "parse()" to get "left" and "right", which are arrays with all the terms
     Send them to "reduce()" to get the reduced form in a dictionnary : 
         reduced = dict("X2" => a, "X1" => b, "X0" => c)
 =#
 function check_polynom(ARGS)
-    array = parse_argv(ARGS)
-    left = array[1]
-    right = array[2]
+    valid = check_chars(ARGS)
+    max_degree = -1
     reduced = Dict()
-    reduce(left, "left", reduced)
-    reduce(right, "right", reduced)
-    print_reduced(reduced)
-    temp = print_degree(reduced)
-    degree = temp[1]
-    valid = temp[2]
-    max_degree = temp[3]
+    if (valid == 1)
+        array = parse_argv(ARGS)
+        left = array[1]
+        right = array[2]
+        reduce(left, "left", reduced)
+        reduce(right, "right", reduced)
+        print_reduced(reduced)
+        temp = print_degree(reduced)
+        degree = temp[1]
+        valid = temp[2]
+        max_degree = temp[3]
+    end
     return (valid, reduced, max_degree)
 end
